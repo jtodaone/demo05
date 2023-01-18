09378214701b267a1e699da376b4f24b0693c3f3
Jaewon Choi <jaewon@thevc.kr>
2023-01-18 18:24:39 +0900

    add article list, turn off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
     modules: [
         '@nuxt/content'
     ],
+    ssr: false
 })
---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="article in data" :key="article.title">
+            <NuxtLink :to="article._path">{{  article.title  }}</NuxtLink>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+    const { data } = await useAsyncData('blog-list', () => queryContent('/blog').find())
+</script>
+
+<style>
+
+</style>
